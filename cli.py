cae47046d6398ebf5e5c012fe0c50d11158870ca
Erich Schroeter <erich.schroeter@gmail.com>
2024-09-08 09:57:54 -0500

    refactor: cli template code

--- cli.py
+++ cli.py
@@ -2,10 +2,12 @@ import argparse
 import csv
 import logging
 import os
+import textwrap
 import yaml
 from openpyxl import Workbook
 
-def main(args):
+
+def excel(args):
     config = None
     if args.config:
         with open(args.config, 'r') as yamlfile:
@@ -47,9 +49,87 @@ def main(args):
     # Save the workbook
     wb.save('output.xlsx')
 
+
+class App:
+    def __init__(self) -> None:
+        self.args = None
+        self.parser = argparse.ArgumentParser(
+            description=textwrap.dedent('''\
+                A commandline utility to manage an Excel file with multiple worksheets while keeping data in CSV files for better Git support.
+                '''),
+            formatter_class=RawTextArgumentDefaultsHelpFormatter)
+        self.parser.add_argument('-v', '--verbosity',
+                                 choices=['critical', 'error', 'warning', 'info', 'debug'],
+                                 default='info',
+                                 help='Set the logging verbosity level.')
+        self.parser.add_argument('-c', '--config',
+                                 help='A YAML configuration file.')
+
+        self.subparsers = self.parser.add_subparsers(dest='command')
+        excel_parser = self.subparsers.add_parser('excel',
+                                                   help='Generate or update an Excel file from multiple CSV files.',
+                                                   formatter_class=RawTextArgumentDefaultsHelpFormatter)
+        excel_parser.add_argument('csv_files', nargs='+', help='The CSV files to include in the Excel file')
+        excel_parser.set_defaults(func=excel)
+
+    def parse_args(self, args=None):
+        self.args = self.parser.parse_args(args)
+
+    def run(self):
+        if not self.args:
+            self.parse_args()
+        _init_logger(getattr(logging, self.args.verbosity.upper()))
+        logging.debug(f'command-line args: {self.args}')
+        self.args.func(self.args)
+
+
+class ColorLogFormatter(logging.Formatter):
+    '''
+    Custom formatter that changes the color of logs based on the log level.
+    '''
+
+    grey = "\x1b[38;20m"
+    green = "\u001b[32m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    blue = "\u001b[34m"
+    cyan = "\u001b[36m"
+    reset = "\x1b[0m"
+
+    timestamp = '%(asctime)s - '
+    loglevel = '%(levelname)s'
+    message = ' - %(message)s'
+
+    FORMATS = {
+        logging.DEBUG:    timestamp + blue + loglevel + reset + message,
+        logging.INFO:     timestamp + green + loglevel + reset + message,
+        logging.WARNING:  timestamp + yellow + loglevel + reset + message,
+        logging.ERROR:    timestamp + red + loglevel + reset + message,
+        logging.CRITICAL: timestamp + bold_red + loglevel + reset + message
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+def _init_logger(level=logging.INFO):
+    logger = logging.getLogger()
+    logger.setLevel(level)
+
+    formatter = ColorLogFormatter()
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+
+class RawTextArgumentDefaultsHelpFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawTextHelpFormatter):
+    pass
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('csv_files', nargs='+', help='The CSV files to include in the Excel file')
-    parser.add_argument('-c', '--config', help='YAML config file')
-    args = parser.parse_args()
-    main(args)
+    App().run()
