412140de933134561c40d64d4ab68ddd8750560b
Erich Schroeter <erichschroeter@gmail.com>
2024-09-30 09:15:06 -0500

    wip: testing validate with unique_id

--- csv_excel/rules/unique_id.py
+++ csv_excel/rules/unique_id.py
@@ -1,3 +1,7 @@
 
 def validate(workbook):
     print(f'VALIDATING : {workbook.filename} for rule "unique id"')
+    for sheet in workbook.worksheets():
+        if 'Parameters' == sheet.name:
+            for row in sheet.table:
+                print(f'{row}')
